a05998bb75fd1792ba77937107d63926d38320d5
nicol3ta <nikk_mihali@yahoo.de>
2015-08-17 16:33:29 +0200

    This closes #1 and closes #2

--- GasFinder/MainPage.xaml.cs
+++ GasFinder/MainPage.xaml.cs
@@ -1,19 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Controls.Maps;
-using Windows.UI.Xaml.Documents;
 using Windows.UI;
 using Windows.Devices.Geolocation;
 using Windows.Storage.Streams;
@@ -311,8 +304,6 @@ namespace GasFinder
                 Debug.WriteLine("Exception: " + e);
             }
         }
-
-
 
         private async void OnStateChanged(object sender, RoutedEventArgs e)
         {
