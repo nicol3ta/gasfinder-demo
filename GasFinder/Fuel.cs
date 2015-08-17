1b54deb9447670fa7a4dc7c4bb6eae1cb649b789
nicol3ta <nikk_mihali@yahoo.de>
2015-08-17 13:27:03 +0200

    Revert "Revert "VoiceCommands added""
    This reverts commit 1a86531f3d9c52842aab877e5d688a748bb27f45.

---
+++ GasFinder/Fuel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GasFinder
+{
+    class Fuel
+    {
+
+        public Fuel(String type, double price)
+        {
+            this.Type = type;
+            this.Price = price;
+        }
+
+        public string Type { get; private set; }
+
+        public double Price { get; private set; }
+    }
+}
